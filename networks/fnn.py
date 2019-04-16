ec381a36a4be50d5fddf4c4212058ab070656a52
Jincheng-Sun <sjc951213@gmail.com>
2019-04-16 19:35:14 -0400

    add networks

--- networks/fnn.py
+++ networks/fnn.py
@@ -4,110 +4,79 @@ from keras.models import load_model
 from keras import Model
 import numpy as np
 from sklearn.externals import joblib
-'''build model'''
-input = Input(shape=(20,))
-layer1 = Dense(64,activation='relu')(input)
-layer2 = Dense(128,activation='relu')(layer1)
-output = Dense(15,activation='softmax')(layer2)
+'''global variance'''
+file_path = 'aftconcat'
+# '''build model'''
+# input = Input(shape=(31,))
+# layer1 = Dense(64,activation='relu')(input)
+# layer2 = Dense(128,activation='relu')(layer1)
+# output = Dense(16,activation='softmax')(layer2)
+#
+# model = Model(input = input, output = output)
+# model.compile(optimizer='adam',loss='categorical_crossentropy',metrics=['accuracy'])
+#
+# '''load data'''
 
-model = Model(input = input, output = output)
-model.compile(optimizer='adam',loss='categorical_crossentropy',metrics=['accuracy'])
+# train_x = np.load('../data/%s/train_x.npy' % file_path)
+# train_y_all = np.load('../data/%s/train_y_all.npy' % file_path)
+#
+# from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+#
+# '''turn label into one-hot form'''
+# labelencoder = LabelEncoder()
+# train_y_all = labelencoder.fit_transform(train_y_all)
+# train_y_all = train_y_all.reshape([-1, 1])
+# ohencoder = OneHotEncoder()
+# train_y_all = ohencoder.fit_transform(train_y_all)
+#
+# '''save encoders'''
+#
+# joblib.dump(labelencoder,'../models/labelencoder')
+# joblib.dump(ohencoder,'../models/OneHotEncoder')
+# del labelencoder,ohencoder
+#
+# '''split train-valid set'''
+# from sklearn.model_selection import train_test_split
+# x_train,x_val,y_train,y_val = train_test_split(train_x, train_y_all, test_size=0.1, random_state=42)
+# del train_x,train_y_all
+#
+#
+# '''train'''
+# from keras.callbacks import EarlyStopping
+#
+#
+# monitor = EarlyStopping(monitor='val_loss',patience=10,restore_best_weights=True)
+# model.fit(x_train,y_train,batch_size=50,epochs=1000,validation_data=(x_val,y_val),callbacks=[monitor])
+# model.save('../models/%s/fnnmodel'%file_path)
+# del model
+# del x_train,x_val,y_train,y_val
 
-'''load data'''
-
-x = np.load('../data/train_x.npy')
-y = np.load('../data/train_y_all.npy')
-
-'''fill 0 with normal'''
-
-y[y == 0 ] = 'normal'
-
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-
-'''turn label into one-hot form'''
-labelencoder = LabelEncoder()
-y = labelencoder.fit_transform(y)
-y = y.reshape([-1,1])
-ohencoder = OneHotEncoder()
-y = ohencoder.fit_transform(y)
-
-'''save encoders'''
-
-joblib.dump(labelencoder,'../models/labelencoder')
-joblib.dump(ohencoder,'../models/OneHotEncoder')
-del labelencoder,ohencoder
-
-'''split train-valid set'''
-from sklearn.model_selection import train_test_split
-x_train,x_val,y_train,y_val = train_test_split(x,y,test_size=0.2,random_state=42)
-del x,y
-
-
-'''train'''
-from keras.callbacks import EarlyStopping
-
-
-monitor = EarlyStopping(monitor='val_loss',patience=10,restore_best_weights=True)
-model.fit(x_train,y_train,batch_size=50,epochs=1000,validation_data=(x_val,y_val),callbacks=[monitor])
-model.save('models/fnnmodel')
-del model
-del x_train,x_val,y_train,y_val
-
-
-
-
-import pandas as pd
 '''load testset and encoders'''
-x_test=np.load('../data/test_x.npy')
-y_test=np.load('../data/test_y_all.npy')
+
+x_test=np.load('../data/%s/test_x.npy'%file_path)
+y_test=np.load('../data/%s/test_y_all.npy'%file_path)
 labelencoder = joblib.load('../models/labelencoder')
 ohencoder = joblib.load('../models/OneHotEncoder')
-model = load_model('../models/fnnmodel')
-y_test[y_test == 0 ] = 'normal'
+model = load_model('../models/%s/fnnmodel'%file_path)
 y_test = labelencoder.transform(y_test).reshape([-1,1])
 label = labelencoder.classes_.tolist()
 
 '''predict'''
+
 y_pred = model.predict(x_test)
 y_pred = np.argmax(y_pred,axis=1).reshape([-1,1])
+
 '''Assessment'''
-from sklearn.metrics import roc_curve, auc, confusion_matrix, classification_report
+
+from sklearn.metrics import confusion_matrix, classification_report
 from sklearn.metrics import accuracy_score
 
 print(classification_report(y_true=y_test,y_pred=y_pred))
 cm = confusion_matrix(y_true=y_test,y_pred=y_pred)
 acc = accuracy_score(y_true=y_test,y_pred=y_pred)
+
 '''Visualization'''
-import seaborn as sns
-import matplotlib.pyplot as plt
-import pandas as pd
 
-def cm_analysis(cm, labels, x_rotation=90, y_rotation=0):
-    cm_sum = np.sum(cm, axis=1, keepdims=True)
-    cm_perc = cm / cm_sum.astype(float) * 100
-    annot = np.empty_like(cm).astype(str)
-    nrows, ncols = cm.shape
-    for i in range(nrows):
-        for j in range(ncols):
-            c = cm[i, j]
-            p = cm_perc[i, j]
-            if i == j:
-                s = cm_sum[i]
-                annot[i, j] = '%.1f%%\n%d/%d' % (p, c, s)
-            elif c == 0:
-                annot[i, j] = ''
-            else:
-                annot[i, j] = '%.1f%%\n%d' % (p, c)
-    cm = pd.DataFrame(cm, index=labels, columns=labels)
-    cm.index.name = 'True Label'
-    cm.columns.name = 'Predict Label'
-    sns.set(font_scale=0.5)
+from toolPackage.draw_cm import cm_analysis
 
-    sns.heatmap(cm, annot=annot, fmt='', cmap='Blues')
-    plt.xticks(rotation=x_rotation)
-    plt.yticks(rotation=y_rotation)
-    plt.rcParams['savefig.dpi'] = 300  # 图片像素
-    plt.rcParams['figure.dpi'] = 300  # 分辨率
-    plt.show()
-
-cm_analysis(cm, label)
+cm_analysis(cm, label, precision=False)
--- networks/one_class_model.py
+++ networks/one_class_model.py
@@ -1,13 +1,15 @@
 from keras import Model
 from keras.layers import Input, Dense
 from keras import Model
-import numpy as np
 from sklearn.externals import joblib
+import numpy as np
+'''global variance'''
+file_path = 'aftconcat'
 
 # '''build model'''
 #
-# input_classification = Input(shape=(20,))
-# input_binary = Input(shape=(20,))
+# input_classification = Input(shape=(31,))
+# input_binary = Input(shape=(31,))
 #
 #
 # def share(input):
@@ -19,7 +21,7 @@ from sklearn.externals import joblib
 # mid_classification = share(input_classification)
 # mid_binary = share(input_binary)
 #
-# output_classification = Dense(15, activation='softmax')(mid_classification)
+# output_classification = Dense(16, activation='softmax')(mid_classification)
 # output_binary = Dense(2, activation='softmax')(mid_binary)
 #
 # model = Model(input=[input_classification, input_binary], output=[output_classification, output_binary])
@@ -27,103 +29,80 @@ from sklearn.externals import joblib
 # model.summary()
 #
 # '''load data'''
-#
-# x = np.load('../data/train_x.npy')
-# y = np.load('../data/train_y_all.npy')
+# file_path = 'aftconcat'
+# train_x = np.load('../data/%s/train_x.npy' % file_path)
+# train_y_all = np.load('../data/%s/train_y_all.npy' % file_path)
+# train_y_bi = np.load('../data/%s/train_y_bi.npy' % file_path)
 #
 # labelencoder = joblib.load('../models/labelencoder')
 # ohencoder = joblib.load('../models/OneHotEncoder')
-# y[y == 0] = 'normal'
-# y = labelencoder.transform(y).reshape([-1, 1])
-# y = ohencoder.fit_transform(y)
+#
+# train_y_all = labelencoder.transform(train_y_all).reshape([-1, 1])
+# train_y_all = ohencoder.fit_transform(train_y_all)
+#
 # del labelencoder, ohencoder
 #
 # from sklearn.preprocessing import OneHotEncoder
 #
-# y2 = np.load('../data/train_y_bi.npy')
 # ohencoder = OneHotEncoder()
-# y2 = ohencoder.fit_transform(y2.reshape([-1, 1]))
+# train_y_bi = ohencoder.fit_transform(train_y_bi.reshape([-1, 1]))
 #
 # '''split dataset'''
 # from sklearn.model_selection import train_test_split
 #
-# x_train, x_val, y_train, y_val = train_test_split(x, y, test_size=0.2, random_state=42)
+# x_train, _, y_train, _ = train_test_split(train_x, train_y_all, test_size=0, random_state=42)
 #
-# x_train2, x_val2, y_train2, y_val2 = train_test_split(x, y2, test_size=0.2, random_state=43)
-# del x, y, y2
+# x_train2, _, y_train2, _ = train_test_split(train_x, train_y_bi, test_size=0, random_state=43)
+# del train_x, train_y_all, train_y_bi
 #
+# #-------------------------------------------------------------------
 # '''train'''
 # from keras.callbacks import EarlyStopping
 #
-# monitor = EarlyStopping(monitor='dense_5_loss', patience=10, restore_best_weights=True)
-# model.fit(x=[x_train, x_train2], y=[y_train, y_train2], batch_size=50, epochs=1000, callbacks=[monitor])
-# model.save('../models/oneclassmodel')
-
+# monitor = EarlyStopping(monitor='val_loss', patience=10, restore_best_weights=True)
+# model.fit(x=[x_train, x_train2], y=[y_train, y_train2], batch_size=50, epochs=1000, callbacks=[monitor],validation_split=0.1)
+# model.save('../models/%s/oneclassmodel'%file_path)
+#
+# # -------------------------------------------------------------------
 import pandas as pd
 from keras.models import load_model
 
+
 '''load testset and encoders'''
-x_test = np.load('../data/test_x.npy')
-y_test = np.load('../data/test_y_all.npy')
-y_test2 = np.load('../data/test_y_bi.npy')
-y_test2 = y_test2.reshape([-1,1])
+x_test = np.load('../data/%s/test_x.npy' % file_path)
+y_test_all = np.load('../data/%s/test_y_all.npy' % file_path)
+y_test_bi = np.load('../data/%s/test_y_bi.npy' % file_path)
 labelencoder = joblib.load('../models/labelencoder')
-model = load_model('../models/oneclassmodel')
-y_test[y_test == 0] = 'normal'
-y_test = labelencoder.transform(y_test).reshape([-1, 1])
+model = load_model('../models/%s/oneclassmodel' % file_path)
+
+y_test_bi = y_test_bi.reshape([-1, 1])
+y_test_all = labelencoder.transform(y_test_all).reshape([-1, 1])
 label = labelencoder.classes_.tolist()
 
 '''predict'''
-y_pred,y_pred2 = model.predict([x_test,x_test])
-y_pred = np.argmax(y_pred, axis=1).reshape([-1, 1])
-y_pred2 = np.argmax(y_pred2, axis=1).reshape([-1, 1])
+y_pred_all, y_pred_bi = model.predict([x_test, x_test])
+y_pred_all = np.argmax(y_pred_all, axis=1).reshape([-1, 1])
+y_pred_bi = np.argmax(y_pred_bi, axis=1).reshape([-1, 1])
 '''Assessment'''
-from sklearn.metrics import roc_curve, auc, confusion_matrix, classification_report
+from sklearn.metrics import confusion_matrix, classification_report
 from sklearn.metrics import accuracy_score
 
-print(classification_report(y_true=y_test, y_pred=y_pred))
-cm = confusion_matrix(y_true=y_test, y_pred=y_pred)
-acc = accuracy_score(y_true=y_test, y_pred=y_pred)
+print(classification_report(y_true=y_test_all, y_pred=y_pred_all))
+cm = confusion_matrix(y_true=y_test_all, y_pred=y_pred_all)
+acc = accuracy_score(y_true=y_test_all, y_pred=y_pred_all)
 '''Visualization'''
 import seaborn as sns
 import matplotlib.pyplot as plt
-import pandas as pd
 
 
-def cm_analysis(cm, labels, x_rotation=90, y_rotation=0):
-    cm_sum = np.sum(cm, axis=1, keepdims=True)
-    cm_perc = cm / cm_sum.astype(float) * 100
-    annot = np.empty_like(cm).astype(str)
-    nrows, ncols = cm.shape
-    for i in range(nrows):
-        for j in range(ncols):
-            c = cm[i, j]
-            p = cm_perc[i, j]
-            if i == j:
-                s = cm_sum[i]
-                annot[i, j] = '%.1f%%\n%d/%d' % (p, c, s)
-            elif c == 0:
-                annot[i, j] = ''
-            else:
-                annot[i, j] = '%.1f%%\n%d' % (p, c)
-    cm = pd.DataFrame(cm, index=labels, columns=labels)
-    cm.index.name = 'True Label'
-    cm.columns.name = 'Predict Label'
-    sns.set(font_scale=0.5)
+from toolPackage.draw_cm import cm_analysis
 
-    sns.heatmap(cm, annot=annot, fmt='', cmap='Blues')
-    plt.xticks(rotation=x_rotation)
-    plt.yticks(rotation=y_rotation)
-    plt.rcParams['savefig.dpi'] = 300  # 图片像素
-    plt.rcParams['figure.dpi'] = 300  # 分辨率
-    plt.show()
-
-
-cm_analysis(cm, label)
+cm_analysis(cm, label, precision=False)
 
 '''for binary'''
-print(classification_report(y_true=y_test2, y_pred=y_pred2))
-cm2 = confusion_matrix(y_true=y_test2, y_pred=y_pred2)
-acc2 = accuracy_score(y_true=y_test2, y_pred=y_pred2)
+print(classification_report(y_true=y_test_bi, y_pred=y_pred_bi))
+cm2 = confusion_matrix(y_true=y_test_bi, y_pred=y_pred_bi)
+acc2 = accuracy_score(y_true=y_test_bi, y_pred=y_pred_bi)
 
-cm_analysis(cm2, ['normal','malfunction'])
+cm_analysis(cm2, ['normal', 'malfunction'], x_rotation=0, font_size=0.5, precision=False)
+
