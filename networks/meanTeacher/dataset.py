400a6cd764ca318c63bc7fb3ccd57982860c7ce1
Jincheng-Sun <sjc951213@gmail.com>
2019-05-09 10:23:29 -0400

    make data array

--- networks/meanTeacher/dataset.py
+++ networks/meanTeacher/dataset.py
@@ -10,13 +10,17 @@ from networks.meanTeacher.minibatching import *
 
 class EuTk:
     def __init__(self):
-        train_x = pd.DataFrame(np.load('../../data/mean_teacher/train_x.npy'))
-        train_x['x'] = train_x.apply(lambda x:x.values,axis = 1)
-        train_x = train_x['x']
-        train_y = pd.DataFrame(np.load('../../data/mean_teacher/train_y.npy'),columns=['y'])
-        training = pd.concat([train_x, train_y], axis = 1)
-        labeled = training[training['y']!=-1]
-        unlabeled = training[training['y']==-1]
+        train_x = np.load('../../data/mean_teacher/train_x.npy')
+        train_y = np.load('../../data/mean_teacher/train_y.npy')
+        train_x = train_x.reshape([-1, 86, 1])
+        array = np.zeros(train_x.shape[0], dtype=[
+            ('x', np.float32, (86, 1)),
+            ('y', np.int32, ())  # We will be using -1 for unlabeled
+        ])
+        array['x'] = train_x
+        array['y'] = train_y
+        labeled = array[array['y']!=-1]
+        unlabeled = array[array['y']==-1]
         lb_train,self.evaluation = train_test_split(labeled,test_size=0.2,random_state=42)
         self.training = pd.concat([lb_train, unlabeled])
 
