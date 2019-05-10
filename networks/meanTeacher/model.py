a0b24ecbae5d8b7e52992f4813e1c1ba2fab28ed
OEM Configuration (temporary user) <sjc951213@gmail.com>
2019-05-10 15:23:26 -0400

    networks

--- networks/meanTeacher/model.py
+++ networks/meanTeacher/model.py
@@ -179,6 +179,10 @@ class Model:
             "train/total_cost/pi": self.mean_total_cost_pi,
             "train/total_cost/mt": self.mean_total_cost_mt,
         }
+        '''monitor values on tensorboard'''
+        for k,v in self.training_metrics.items():
+            tf.summary.scalar(k,v)
+
         with tf.variable_scope("validation_metrics") as metrics_scope:
             self.metric_values, self.metric_update_ops = metrics.aggregate_metric_map({
                 "eval/error/1": streaming_mean(self.errors_1),
@@ -205,7 +209,7 @@ class Model:
             self.init_init_op = tf.variables_initializer(init_init_variables)
             self.train_init_op = tf.variables_initializer(train_init_variables)
 
-        self.saver = tf.train.Saver()
+        self.saver = tf.train.Saver(max_to_keep=10)
         self.session = tf.Session()
         self.run(self.init_init_op)
 
@@ -216,22 +220,29 @@ class Model:
         return self.hyper.get(self.session, key)
 
     def train(self, training_batches, evaluation_batches_fn):
+        self.best_loss = 1
+        self.patience = 0
         self.run(self.train_init_op, self.feed_dict(next(training_batches)))
         LOG.info("Model variables initialized")
         self.evaluate(evaluation_batches_fn)
         self.save_checkpoint()
+        merged = tf.summary.merge_all()
         for batch in training_batches:
-            results, _ = self.run([self.training_metrics, self.train_step_op],
+            merge, i, results, _ = self.run([merged,self.global_step, self.training_metrics, self.train_step_op],
                                   self.feed_dict(batch))
+            self.writer.add_summary(merge, i)
             step_control = self.get_training_control()
             if step_control['time_to_print']:
                 LOG.info("step %5d:   %s", step_control['step'], self.result_formatter.format_dict(results))
             if step_control['time_to_stop']:
                 break
             if step_control['time_to_evaluate']:
-                self.evaluate(evaluation_batches_fn)
+                if_stop = self.evaluate(evaluation_batches_fn)
                 self.save_checkpoint()
-        self.evaluate(evaluation_batches_fn)
+                if if_stop:
+                    LOG.info('Stop Training')
+                    break
+        _ = self.evaluate(evaluation_batches_fn)
         self.save_checkpoint()
 
     def evaluate(self, evaluation_batches_fn):
@@ -241,7 +252,23 @@ class Model:
                      feed_dict=self.feed_dict(batch, is_training=False))
         step = self.run(self.global_step)
         results = self.run(self.metric_values)
+        '''early stoping'''
+        loss = results["eval/error/1"]
+        if loss <= self.best_loss:
+            self.best_loss = loss
+            self.patience = 0
+        else:
+            self.patience += 1
+
+        if self.patience == 8:
+            stop_training = True
+        else:
+            stop_training = False
+
         LOG.info("step %5d:   %s", step, self.result_formatter.format_dict(results))
+
+        return stop_training
+
 
     def get_training_control(self):
         return self.session.run(self.training_control)
@@ -261,9 +288,9 @@ class Model:
         LOG.info("Saved checkpoint: %r", path)
 
     def save_tensorboard_graph(self):
-        writer = tf.summary.FileWriter(self.tensorboard_path)
-        writer.add_graph(self.session.graph)
-        return writer.get_logdir()
+        self.writer = tf.summary.FileWriter(self.tensorboard_path)
+        self.writer.add_graph(self.session.graph)
+        return self.writer.get_logdir()
 
     def restore_checkpoint(self, number):
         self.saver.restore(self.session, self.checkpoint_path+'-%s'%str(number))
@@ -274,6 +301,7 @@ class Model:
             feed_dict = {self.features: test_x, self.labels:test_y, self.is_training: False},
         )
         return class_logits_1, class_logits_2, class_logits_ema
+
 
 
 # ----------------------------------------------------------------------------------------------------------------------
--- networks/meanTeacher/train.py
+++ networks/meanTeacher/train.py
@@ -2,30 +2,35 @@ from networks.meanTeacher.model import Model
 from networks.meanTeacher import minibatching
 from networks.meanTeacher.dataset import EuTk
 import numpy as np
-from toolPackage.draw_cm import cm_metrix, cm_analysis
-from sklearn.externals import joblib
+
 model = Model()
 model['rampdown_length'] = 0
 model['rampup_length'] = 5000
-model['training_length'] = 40000
+model['training_length'] = 20000
 model['max_consistency_cost'] = 50.0
 
 tensorboard_dir = model.save_tensorboard_graph()
 
 eutk = EuTk()
 print('finish loading dataset')
-# training_batches = minibatching.training_batches(eutk.training, n_labeled_per_batch=50)
-# evaluation_batches_fn = minibatching.evaluation_epoch_generator(eutk.evaluation)
+training_batches = minibatching.training_batches(eutk.training, n_labeled_per_batch=50)
+evaluation_batches_fn = minibatching.evaluation_epoch_generator(eutk.evaluation)
 
 # model.train(training_batches, evaluation_batches_fn)
 
+from toolPackage.draw_cm import cm_metrix, cm_analysis
+from sklearn.externals import joblib
+from sklearn.metrics import accuracy_score,classification_report
 test_x = eutk.evaluation['x']
 test_y = eutk.evaluation['y']
-model.restore_checkpoint(38000)
+model.restore_checkpoint(6500)
 result1,result2,result3 = model.test(test_x,test_y)
 result = np.argmax(result1,axis=1)
 result = result.reshape([-1,1])
 cm = cm_metrix(test_y,result)
-labelencoder = joblib.load('../../models/labelencoder')
+labelencoder = joblib.load('../../models/LE_ALM')
 labels = labelencoder.classes_.tolist()
 cm_analysis(cm,labels)
+
+acc = accuracy_score(test_y,result)
+print(classification_report(test_y,result))
