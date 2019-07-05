4a6c6c3ea4e47db0917adfb9a674a44c21ddf11f
OEM Configuration (temporary user) <sjc951213@gmail.com>
2019-07-04 23:06:28 -0400

    change ckpt_path

--- networks/attention/attention_train.py
+++ networks/attention/attention_train.py
@@ -11,7 +11,7 @@ dataset = Attn_dataset(feature_path='/home/oem/Projects/NetDeviceAbnormalDetecti
                        dev_path= '/home/oem/Projects/NetDeviceAbnormalDetection/data/attention/c_dev',
                        label_path='/home/oem/Projects/NetDeviceAbnormalDetection/data/attention/c_alm')
 resnet_1d = Resnet_1d()
-model = Attn_model(ckpt_path='model/attn', tsboard_path='log/', network=resnet_1d,input_shape=[45, 1],num_classes=12,
+model = Attn_model(ckpt_path='models/attn', tsboard_path='log/', network=resnet_1d,input_shape=[45, 1],num_classes=12,
                    feature_num=45, dev_num=11, lr = 0.001, batch_size=100)
 model.initialize_variables()
 model.save_tensorboard_graph()
