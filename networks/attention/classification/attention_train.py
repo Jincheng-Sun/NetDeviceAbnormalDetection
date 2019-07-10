18c05f8339622f45be01f939bcaee73c4b5939e6
OEM Configuration (temporary user) <sjc951213@gmail.com>
2019-07-10 18:53:20 -0400

    update evaluation

--- networks/attention/classification/attention_train.py
+++ networks/attention/classification/attention_train.py
@@ -6,37 +6,13 @@ from Networks.residual_network_1d import Resnet_1d
 from evaluation.metrics import metrics_binary, metrics_multi, auc_roc
 from visualization.draw_matrix import draw_confusion_matrix
 from visualization.draw_roc import plot_roc_curve
+from networks.attention.results_process import visualize_proba
 import numpy as np
-
-dataset = Attn_dataset(feature_path='data/a_PMs',
-                       dev_path= 'data/a_dev',
-                       label_path='data/a_alm',
-                       out_num=12)
-resnet_1d = Resnet_1d()
-model = Attn_model(ckpt_path='models/attn', tsboard_path='log/', network=resnet_1d,input_shape=[45, 1],
-                   num_classes=12, feature_num=45, dev_num=11, lr=0.001, batch_size=100,
-                   regression=False)
-model.initialize_variables()
-model.save_tensorboard_graph()
-model.train(dataset)
-
-model.restore_checkpoint(954)
-prediction = model.get_prediction(dataset, is_training = True).reshape([-1,1])
-
 
 alarm_list = ['Excessive Error Ratio', 'Frequency Out Of Range', 'GCC0 Link Failure',
               'Gauge Threshold Crossing Alert Summary', 'Link Down', 'Local Fault',
               'Loss Of Clock', 'Loss Of Frame', 'Loss Of Signal', 'OSC OSPF Adjacency Loss',
               'OTU Signal Degrade', 'Rx Power Out Of Range']
-
-cm, accuracy = metrics_multi(
-    y_pred=prediction, y_test=np.argmax(dataset.test_set['y'], axis=1), labels=alarm_list)
-
-draw_confusion_matrix(cm, alarm_list, precision=True)
-
-test_dev = np.diag(np.ones([11]))
-attn1, attn2 = model.get_attn_matrix(test_dev)
-
 
 dev_list = ['AMP', 'ETH10G', 'ETHN', 'ETTP', 'OC192', 'OPTMON', 'OSC', 'OTM', 'OTM2', 'OTUTTP', 'PTP']
 
@@ -55,6 +31,67 @@ pm_list = ['BBE-RS',
             'QAVG-OTU', 'QSTDEV-OTU',
             'RS-BBE', 'RS-ES',
             'S-CV', 'S-ES']
+
+dataset = Attn_dataset(feature_path='data/a_PMs',
+                       dev_path= 'data/a_dev',
+                       label_path='data/a_alm',
+                       out_num=12)
+resnet_1d = Resnet_1d()
+model = Attn_model(ckpt_path='models/attn', tsboard_path='log/', network=resnet_1d,input_shape=[45, 1],
+                   num_classes=12, feature_num=45, dev_num=11, lr=0.001, batch_size=100,
+                   regression=False)
+# model.initialize_variables()
+# model.save_tensorboard_graph()
+# model.train(dataset)
+
+model.restore_checkpoint(6731)
+
+# -------------------------------------------------------------------------------------------
+# overall
+# -------------------------------------------------------------------------------------------
+
+prediction = model.get_prediction(dataset, is_training = True).reshape([-1,1])
+
+cm, accuracy = metrics_multi(
+    y_pred=prediction, y_test=np.argmax(dataset.test_set['y'], axis=1), labels=alarm_list)
+
+draw_confusion_matrix(cm, alarm_list, precision=True)
+
+# # -------------------------------------------------------------------------------------------
+# # per class
+# # -------------------------------------------------------------------------------------------
+#
+# # threshold = 0.9
+# index = 0
+#
+# proba = model.get_proba(dataset)
+# real = dataset.test_set['y']
+#
+# proba_i = proba[:, index]
+# real_i = real[:, index]
+# import collections
+# print(collections.Counter(real_i))
+# auc, fprs, tprs, thresholds = auc_roc(y_pred=proba_i, y_test=real_i)
+#
+# plot_roc_curve(fprs, tprs, auc, x_axis=1)
+#
+# proba = visualize_proba(proba, alarm_list)
+#
+# # cm, fpr, acc, precision, recall = metrics_binary(
+# #     y_pred=proba_i, y_test=real_i, threshold=threshold)
+#
+# # draw_confusion_matrix(cm, ['Normal', 'malfunction'], precision=True)
+
+
+# -------------------------------------------------------------------------------------------
+# attn matrix
+# -------------------------------------------------------------------------------------------
+
+test_dev = np.diag(np.ones([11]))
+attn1, attn2 = model.get_attn_matrix(test_dev)
+
+
+
 
 
 #
--- networks/attention/classification/concat_train.py
+++ networks/attention/classification/concat_train.py
@@ -6,7 +6,13 @@ from Networks.residual_network_1d import Resnet_1d
 from evaluation.metrics import metrics_binary, metrics_multi, auc_roc
 from visualization.draw_matrix import draw_confusion_matrix
 from visualization.draw_roc import plot_roc_curve
+from networks.attention.results_process import visualize_proba
 import numpy as np
+
+alarm_list = ['Excessive Error Ratio', 'Frequency Out Of Range', 'GCC0 Link Failure',
+              'Gauge Threshold Crossing Alert Summary', 'Link Down', 'Local Fault',
+              'Loss Of Clock', 'Loss Of Frame', 'Loss Of Signal', 'OSC OSPF Adjacency Loss',
+              'OTU Signal Degrade', 'Rx Power Out Of Range']
 
 dataset = CNN_dataset(feature_path='data/c_PMs',
                       label_path='data/c_alm',
@@ -15,21 +21,50 @@ resnet_1d = Resnet_1d()
 model = CNN_model(ckpt_path='models/conc', tsboard_path='log/', network=resnet_1d,
                   input_shape=[56, 1],num_classes=12,
                    lr = 0.001, batch_size=100, regression = False)
-model.initialize_variables()
-model.save_tensorboard_graph()
-model.train(dataset)
+# model.initialize_variables()
+# model.save_tensorboard_graph()
+# model.train(dataset)
 
-model.restore_checkpoint(954)
+model.restore_checkpoint(2279)
+
+# -------------------------------------------------------------------------------------------
+# overall
+# -------------------------------------------------------------------------------------------
+
 prediction = model.get_prediction(dataset).reshape([-1,1])
 
 
-alarm_list = ['Excessive Error Ratio', 'Frequency Out Of Range', 'GCC0 Link Failure',
-              'Gauge Threshold Crossing Alert Summary', 'Link Down', 'Local Fault',
-              'Loss Of Clock', 'Loss Of Frame', 'Loss Of Signal', 'OSC OSPF Adjacency Loss',
-              'OTU Signal Degrade', 'Rx Power Out Of Range']
 
 cm, accuracy = metrics_multi(
     y_pred=prediction, y_test=np.argmax(dataset.test_set['y'], axis=1), labels=alarm_list)
 
 draw_confusion_matrix(cm, alarm_list, precision=True)
+
+
+
+# # -------------------------------------------------------------------------------------------
+# # per class
+# # -------------------------------------------------------------------------------------------
+#
+# # threshold = 0.9
+# index = 3
+#
+# proba = model.get_proba(dataset)
+# real = dataset.test_set['y']
+#
+# proba_i = proba[:, index]
+# real_i = real[:, index]
+# import collections
+# print(collections.Counter(real_i))
+# auc, fprs, tprs, thresholds = auc_roc(y_pred=proba_i, y_test=real_i)
+#
+# plot_roc_curve(fprs, tprs, auc, x_axis=1)
+#
+# proba = visualize_proba(proba, alarm_list)
+#
+# # cm, fpr, acc, precision, recall = metrics_binary(
+# #     y_pred=proba_i, y_test=real_i, threshold=threshold)
+#
+# # draw_confusion_matrix(cm, ['Normal', 'malfunction'], precision=True)
+
 
