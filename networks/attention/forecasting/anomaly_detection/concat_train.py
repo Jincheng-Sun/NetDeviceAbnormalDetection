8a8794f9decf23d8f04f7de8d93891bb3e580176
OEM Configuration (temporary user) <sjc951213@gmail.com>
2019-07-17 22:03:00 -0400

    training process of concat model, same flow with attention model

--- networks/attention/forecasting/anomaly_detection/concat_train.py
+++ networks/attention/forecasting/anomaly_detection/concat_train.py
@@ -7,7 +7,7 @@ from evaluation.metrics import metrics_binary, auc_roc
 from visualization.draw_matrix import draw_confusion_matrix
 from visualization.draw_roc import plot_roc_curve
 import numpy as np
-threshold = 0.8
+threshold = 0.99
 m = 5
 n = 2
 dataset = Concat_dataset(feature_path='data/m%s_n%s_attn_features.npy'%(m, n),
@@ -16,7 +16,7 @@ dataset = Concat_dataset(feature_path='data/m%s_n%s_attn_features.npy'%(m, n),
                        out_num=1)
 resnet_2d = Resnet_2d()
 
-model = CNN_model(ckpt_path='models/conc', tsboard_path='log/', network=resnet_2d,
+model = CNN_model(ckpt_path='models/conc_m%s'%m, tsboard_path='log/', network=resnet_2d,
                   input_shape=[m, 56, 1],num_classes=1,
                    lr = 0.001, batch_size=100, regression = True, threshold=threshold,
                   patience = 20)
@@ -25,41 +25,41 @@ model.initialize_variables()
 model.save_tensorboard_graph()
 model.train(dataset)
 
-# model.restore_checkpoint(610)
-# prediction = model.get_proba(dataset)
-#
-# auc, fprs, tprs, thresholds = auc_roc(y_pred=prediction, y_test=dataset.test_set['y'])
-#
-# plot_roc_curve(fprs, tprs, auc, x_axis=0.05)
-#
-# cm, fpr, acc, precision, recall = metrics_binary(
-#     y_pred=prediction, y_test=dataset.test_set['y'],threshold=threshold)
-#
-# draw_confusion_matrix(cm, ['Normal', 'malfunction'], precision=True)
-#
-# test_dev = np.diag(np.ones([11]))
-# attn1, attn2 = model.get_attn_matrix(test_dev)
+model.restore_checkpoint(984)
+prediction = model.get_proba(dataset)
+
+auc, fprs, tprs, thresholds = auc_roc(y_pred=prediction, y_test=dataset.test_set['y'])
+
+plot_roc_curve(fprs, tprs, auc, x_axis=0.05)
+
+cm, fpr, acc, precision, recall = metrics_binary(
+    y_pred=prediction, y_test=dataset.test_set['y'],threshold=threshold)
+
+draw_confusion_matrix(cm, ['Normal', 'Anomaly'], precision=True, font_size = 0.8)
+
+test_dev = np.diag(np.ones([11]))
+attn1, attn2 = model.get_attn_matrix(test_dev)
+
+
+dev_list = ['AMP', 'ETH10G', 'ETHN', 'ETTP', 'OC192', 'OPTMON', 'OSC', 'OTM', 'OTM2', 'OTUTTP', 'PTP']
+
+pm_list = ['BBE-RS',
+            'CV-OTU', 'CV-S',
+            'DROPGAINAVG-OTS', 'DROPGAINMAX-OTS_DROPGAINMIN-OTS_-',
+            'E-CV', 'E-ES', 'E-INFRAMESERR_E-INFRAMES_/', 'E-OUTFRAMESERR_E-OUTFRAMES_/',
+            'E-UAS', 'ES-OTU', 'ES-RS', 'ES-S',
+            'OCH-OPRAVG', 'OCH-OPRMAX_OCH-OPRMIN_-', 'OCH-SPANLOSSAVG', 'OCH-SPANLOSSMAX_OCH-SPANLOSSMIN_-',
+            'OPINAVG-OTS', 'OPINMAX-OTS_OPINMIN-OTS_-',
+            'OPOUTAVG-OTS', 'OPOUTAVG-OTS_OPINAVG-OTS_-', 'OPOUTMAX-OTS_OPOUTMIN-OTS_-',
+            'OPRAVG-OCH', 'OPRAVG-OTS', 'OPRMAX-OCH_OPRMIN-OCH_-', 'OPRMAX-OTS_OPRMIN-OTS_-',
+            'OPTAVG-OCH', 'OPTAVG-OTS', 'OPTMAX-OCH_OPTMIN-OCH_-', 'OPTMAX-OTS_OPTMIN-OTS_-',
+            'ORLAVG-OTS', 'ORLMIN-OTS', 'OTU-CV', 'OTU-ES', 'OTU-QAVG', 'OTU-QSTDEV',
+            'PCS-CV', 'PCS-ES', 'PCS-UAS',
+            'QAVG-OTU', 'QSTDEV-OTU',
+            'RS-BBE', 'RS-ES',
+            'S-CV', 'S-ES']
+
 
 #
-# dev_list = ['AMP', 'ETH10G', 'ETHN', 'ETTP', 'OC192', 'OPTMON', 'OSC', 'OTM', 'OTM2', 'OTUTTP', 'PTP']
-#
-# pm_list = ['BBE-RS',
-#             'CV-OTU', 'CV-S',
-#             'DROPGAINAVG-OTS', 'DROPGAINMAX-OTS_DROPGAINMIN-OTS_-',
-#             'E-CV', 'E-ES', 'E-INFRAMESERR_E-INFRAMES_/', 'E-OUTFRAMESERR_E-OUTFRAMES_/',
-#             'E-UAS', 'ES-OTU', 'ES-RS', 'ES-S',
-#             'OCH-OPRAVG', 'OCH-OPRMAX_OCH-OPRMIN_-', 'OCH-SPANLOSSAVG', 'OCH-SPANLOSSMAX_OCH-SPANLOSSMIN_-',
-#             'OPINAVG-OTS', 'OPINMAX-OTS_OPINMIN-OTS_-',
-#             'OPOUTAVG-OTS', 'OPOUTAVG-OTS_OPINAVG-OTS_-', 'OPOUTMAX-OTS_OPOUTMIN-OTS_-',
-#             'OPRAVG-OCH', 'OPRAVG-OTS', 'OPRMAX-OCH_OPRMIN-OCH_-', 'OPRMAX-OTS_OPRMIN-OTS_-',
-#             'OPTAVG-OCH', 'OPTAVG-OTS', 'OPTMAX-OCH_OPTMIN-OCH_-', 'OPTMAX-OTS_OPTMIN-OTS_-',
-#             'ORLAVG-OTS', 'ORLMIN-OTS', 'OTU-CV', 'OTU-ES', 'OTU-QAVG', 'OTU-QSTDEV',
-#             'PCS-CV', 'PCS-ES', 'PCS-UAS',
-#             'QAVG-OTU', 'QSTDEV-OTU',
-#             'RS-BBE', 'RS-ES',
-#             'S-CV', 'S-ES']
-#
-#
-# #
-# from networks.attention.results_process import visualize_input_attention
-# attn1 = visualize_input_attention(attn1, dev_list, pm_list)
+from networks.attention.results_process import visualize_proba
+proba = visualize_proba(prediction, alarm_list=['normal', 'anomaly'])
