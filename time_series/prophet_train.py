c58f57cf18c1ed2cdb62cdab4bdce0f0a70a2683
OEM Configuration (temporary user) <sjc951213@gmail.com>
2019-09-03 20:05:19 -0400

    time series

--- time_series/prophet_train.py
+++ time_series/prophet_train.py
@@ -4,6 +4,8 @@ from os import listdir
 from os.path import isfile, join
 import fastparquet as fp
 import matplotlib.pyplot as plt
+from time_series.sort_data_15m import plot_per_device
+import numpy as np
 
 status_list = ['IS', 'IS-ANR', 'n/a'
                ]
@@ -37,8 +39,43 @@ df = df[df['ID'].isin(oos_ratio)]
 df.drop(['oos'], axis = 1, inplace = True)
 
 otm4 = df[df['meta_facility'] == 'OTM4'].fillna(0)
+otm4_copy = otm4.copy()
 
+# array(['Node19:OTM4-2-1-1', 'Node363:OTM4-1-1-1', 'Node207:OTM4-1-1-1',
+#        'Node339:OTM4-1-1-1', 'Node279:OTM4-1-17-1', 'Node77:OTM4-1-5-1',
+#        'Node406:OTM4-6-7-1', 'Node13:OTM4-1-7-1', 'Node173:OTM4-2-3-1',
+#        'Node240:OTM4-2-5-1', 'Node150:OTM4-1-1-1', 'Node141:OTM4-2-5-1',
+#        'Node368:OTM4-1-10-1', 'Node64:OTM4-1-7-1', 'Node240:OTM4-3-1-1',
+#        'Node71:OTM4-2-2-1', 'Node266:OTM4-1-15-1', 'Node173:OTM4-2-4-1',
+#        'Node148:OTM4-1-1-1', 'Node77:OTM4-1-1-1', 'Node150:OTM4-2-1-1',
+#        'Node222:OTM4-1-5-1', 'Node406:OTM4-6-5-1', 'Node306:OTM4-1-1-1',
+#        'Node115:OTM4-3-5-1', 'Node141:OTM4-2-3-1', 'Node173:OTM4-1-1-1',
+#        'Node13:OTM4-1-28-1', 'Node19:OTM4-1-1-1', 'Node17:OTM4-1-4-1',
+#        'Node275:OTM4-1-7-1', 'Node419:OTM4-3-1-1', 'Node303:OTM4-1-1-1',
+#        'Node352:OTM4-1-8-1', 'Node249:OTM4-1-4-1', 'Node222:OTM4-1-1-1',
+#        'Node240:OTM4-2-1-1', 'Node115:OTM4-3-6-1', 'Node196:OTM4-1-1-1',
+#        'Node363:OTM4-1-3-1', 'Node13:OTM4-1-7-2', 'Node77:OTM4-1-9-1',
+#        'Node363:OTM4-1-2-1', 'Node266:OTM4-1-5-1', 'Node196:OTM4-2-5-1',
+#        'Node141:OTM4-1-3-1', 'Node406:OTM4-1-7-1', 'Node85:OTM4-1-4-1',
+#        'Node115:OTM4-2-1-1', 'Node324:OTM4-2-4-1', 'Node214:OTM4-1-3-1',
+#        'Node419:OTM4-3-4-1', 'Node173:OTM4-1-4-1', 'Node148:OTM4-1-4-1',
+#        'Node173:OTM4-1-3-1', 'Node85:OTM4-1-4-2', 'Node249:OTM4-2-4-1',
+#        'Node249:OTM4-1-1-1', 'Node363:OTM4-1-4-1', 'Node275:OTM4-1-5-1',
+#        'Node17:OTM4-2-2-1', 'Node266:OTM4-3-3-1', 'Node420:OTM4-2-5-1',
+#        'Node115:OTM4-2-4-1', 'Node380:OTM4-1-1-1', 'Node249:OTM4-2-1-1',
+#        'Node279:OTM4-1-17-2', 'Node240:OTM4-1-1-1', 'Node214:OTM4-1-11-1',
+#        'Node385:OTM4-1-3-1', 'Node412:OTM4-1-1-1', 'Node347:OTM4-2-1-1'],
+#       dtype=object)
+
+# anomaly = otm4[otm4['category'] != 0]
+# array(['Node207:OTM4-1-1-1', 'Node279:OTM4-1-17-1', 'Node13:OTM4-1-7-1',
+#        'Node240:OTM4-2-5-1', 'Node240:OTM4-3-1-1', 'Node115:OTM4-3-6-1',
+#        'Node13:OTM4-1-7-2', 'Node115:OTM4-2-1-1', 'Node85:OTM4-1-4-2',
+#        'Node115:OTM4-2-4-1', 'Node279:OTM4-1-17-2', 'Node240:OTM4-1-1-1'],
+#       dtype=object)
 keep_pms = ['OTU-QAVG', 'OTU-ES', 'OPRMAX-OCH_OPRMIN-OCH_-', 'OPRAVG-OCH', 'OTU-QSTDEV', 'CV-OTU']
+
+otm4 = otm4[['ID', 'TIME'] + keep_pms]
 
 def plot_original(data, Device):
     plt.rcParams['figure.figsize'] = [8, 6]
@@ -55,8 +92,10 @@ def plot_original(data, Device):
 
 def plot(data, Device='Node385:OTM4-1-3-1', PM='OTU-QAVG', train_portion=0.9):
 
-    # keep columns
-    data = data[['ID', 'TIME'] + [PM]]
+    additional_pms = ['OTU-QAVG', 'OTU-ES', 'OPRMAX-OCH_OPRMIN-OCH_-', 'OPRAVG-OCH', 'OTU-QSTDEV', 'CV-OTU']
+    additional_pms.remove(PM)
+    # # keep columns
+    # data = data[['ID', 'TIME'] + [PM]]
 
     per_device = data[data['ID'] == Device]
 
@@ -64,25 +103,42 @@ def plot(data, Device='Node385:OTM4-1-3-1', PM='OTU-QAVG', train_portion=0.9):
 
     m = Prophet()
 
-    dt = per_device[:int(per_device.shape[0] * train_portion)]
+    for i in additional_pms:
+        m.add_regressor(i)
+
+    dt = per_device[:int(per_device.shape[0] * train_portion)].copy()
+
     m.fit(dt)
 
     future = m.make_future_dataframe(periods=per_device.shape[0] - int(per_device.shape[0] * train_portion),
                                      freq='15min', include_history=True)
+    for i in additional_pms:
+        future[i] = pd.Series(dt[i].to_list())
 
     forecast = m.predict(future)
 
     plt.rcParams['figure.figsize'] = [8, 6]
     plt.rcParams['figure.dpi'] = 200
 
-    per_device.set_index('ds', inplace=True)
-    per_device.index = pd.to_datetime(per_device.index)
-    per_device['y'].plot()
-    plt.show()
 
     fig1 = m.plot(forecast)
     fig2 = m.plot_components(forecast)
     fig1.show()
     fig2.show()
+
+    plt.cla()
+
+    per_device.set_index('ds', inplace=True)
+    per_device.index = pd.to_datetime(per_device.index)
+    per_device = per_device.reindex(pd.date_range('20190523', end='201906232100', freq='15T'))
+
+    pd.plotting.register_matplotlib_converters()
+
+    per_device['y'].plot()
+
+    plt.show()
+    plt.cla()
+    return per_device['y']
+
 
 
--- time_series/simple_lstm_train.py
+++ time_series/simple_lstm_train.py
@@ -1,40 +1,69 @@
 from keras.models import Sequential
-from keras.layers import Dense, LSTM, Dropout, TimeDistributed, Activation
+from keras.layers import Dense, LSTM, Dropout, TimeDistributed, Activation, SimpleRNN
 from keras.optimizers import SGD, Adam, RMSprop
 from keras.callbacks import EarlyStopping
 import numpy as np
 
-features_train = np.load('data/OTM4_features_96x6_train.npy')
-targets_train = np.load('data/OTM4_targets_96x6_train.npy')
-
-features_test = np.load('data/OTM4_features_96x6_test.npy')
-targets_test = np.load('data/OTM4_targets_96x6_test.npy')
-
+# features_train = np.load('data/OTM4_features_96x6_train.npy')
+# targets_train = np.load('data/OTM4_targets_96x6_train.npy')
+#
+# features_test = np.load('data/OTM4_features_96x6_test.npy')
+# targets_test = np.load('data/OTM4_targets_96x6_test.npy')
+#
+# # train one target
+# index = 0
+# targets_train = targets_train[:, :, index]
+# targets_train = np.expand_dims(targets_train, -1)
+# targets_test = targets_test[:, :, index]
+# targets_test = np.expand_dims(targets_test, -1)
+#
+#
+# # design network
+# model = Sequential()
+# model.add(LSTM(units=64, input_shape=(features_train.shape[1], features_train.shape[2]), return_sequences=True))
+# model.add(Dropout(0.2))
+# # model.add(LSTM(units=128, return_sequences=True))
+# # model.add(Dropout(0.2))
+# # model.add(TimeDistributed(Dense(features_train.shape[2])))
+# # model.add(TimeDistributed(Dense(1)))
+# model.add(Dense(1))
+#
+# # model.add(Activation("tanh"))
+#
+# print(model.summary())
+#
+# model.compile(loss='mae', optimizer='rmsprop')
+# # fit network
+#
+# early_stop = EarlyStopping(patience=5, restore_best_weights=True)
+# history = model.fit(features_train, targets_train, epochs=100, batch_size=50,
+#                     validation_split=0.2, shuffle=True, callbacks=[early_stop]
+#                     )
+# # 2 epoches
+#
+# from matplotlib import pyplot
+# # plot history
+# pyplot.plot(history.history['loss'], label='train')
+# pyplot.plot(history.history['val_loss'], label='test')
+# pyplot.legend()
+# pyplot.show()
+#
+# model.save('models/OTM4_model')
 
 # design network
-model = Sequential()
-model.add(LSTM(units=64, input_shape=(features_train.shape[1], features_train.shape[2]), return_sequences=True))
-model.add(Dropout(0.2))
-# model.add(LSTM(units=128, return_sequences=True))
+# timesteps = 10
+# features = 5
+# model = Sequential()
+# model.add(SimpleRNN(20, input_shape=(timesteps, features), return_sequences=True))
 # model.add(Dropout(0.2))
-model.add(TimeDistributed(Dense(features_train.shape[2])))
-# model.add(Activation("tanh"))
-
-print(model.summary())
-model.compile(loss='mae', optimizer='rmsprop')
-# fit network
-
-early_stop = EarlyStopping(patience=5, restore_best_weights=True)
-history = model.fit(features_train, targets_train, epochs=100, batch_size=50,
-                    validation_split=0.2, shuffle=True, callbacks=[early_stop]
-                    )
-# 2 epoches
-
-from matplotlib import pyplot
-# plot history
-pyplot.plot(history.history['loss'], label='train')
-pyplot.plot(history.history['val_loss'], label='test')
-pyplot.legend()
-pyplot.show()
-
-model.save('models/OTM4_model')
+# # model.add(LSTM(units=128, return_sequences=True))
+# # model.add(Dropout(0.2))
+# # model.add(TimeDistributed(Dense(6)))
+# model.add(Dense(6))
+# # model.add(Activation("tanh"))
+#
+# print(model.summary())
+#
+# model = Sequential()
+# model.add(Dense(4, input_shape=(10, 5)))
+# print(model.summary())
